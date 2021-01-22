0975ad1721801e327696e398880481e205b43150
thanhtrung2511 <18520179@gm.uit.edu.vn>
2021-01-22 17:21:21 +0700

    Change something in roomchat

--- TickNTalk/screens/RoomChatManagement.js
+++ TickNTalk/screens/RoomChatManagement.js
@@ -1,13 +1,18 @@
 import React from "react";
 import {
-  
   Text,
   SafeAreaView,
-  View,ScrollView,
+  View,
+  ScrollView,
   KeyboardAvoidingView,
 } from "react-native";
 
-import { styles, ButtonIcon, colors } from "../components/Basic/Basic";
+import {
+  styles,
+  ButtonIcon,
+  colors,
+  ButtonMod,
+} from "../components/Basic/Basic";
 import { SearchBar } from "react-native-elements";
 
 export default class RoomChatManagements extends React.Component {
@@ -16,6 +21,7 @@ export default class RoomChatManagements extends React.Component {
     password: "",
     repassword: "",
     Email: "",
+    onCreate: false,
   };
   ChatScreenNav = () => {
     this.props.navigation.navigate("Chat");
@@ -23,7 +29,7 @@ export default class RoomChatManagements extends React.Component {
   render() {
     return (
       <SafeAreaView style={styles.containerLI}>
-        <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <KeyboardAvoidingView style={[styles.container,!this.state.onCreate?{justifyContent:"space-between"}:null]} behavior="padding">
           <View
             style={{
               backgroundColor: colors.lightpink,
@@ -37,14 +43,15 @@ export default class RoomChatManagements extends React.Component {
               flexDirection="row"
             >
               <Text style={styles.header}>Nhóm</Text>
-              <ButtonIcon MaterialFamilyIconName="group-add" size={33} />
+              {this.state.onCreate?<ButtonIcon MaterialFamilyIconName="done-all" color={"whitesmoke"} onPress={()=>this.setState({onCreate:false})} size={33} />:null}
             </View>
           </View>
-          <View
-            style={{ flexDirection: "column" }}
-            justifyContent="space-between"
-          >
-            {/* <TextInput style={styles.input}
+          {this.state.onCreate ? (
+            <View
+              style={{ flexDirection: "column" }}
+              justifyContent="space-between"
+            >
+              {/* <TextInput style={styles.input}
                       placeholder="Tìm kiếm bạn bè.."
                       // clearButtonMode={Platform.OS === "ios" ? true:false}
                       onChangeText={Text=>{
@@ -52,31 +59,43 @@ export default class RoomChatManagements extends React.Component {
                       }}>
                 </TextInput> */}
 
-            <ScrollView style={{ maxHeight: "94%" }}>
-              <SearchBar
-                platform={Platform.OS}
-                placeholder="Tìm bạn bè..."
-                lightTheme="true"
-                containerStyle={{
-                  marginHorizontal: 8,
-                  backgroundColor: "transparent",
-                  width: "95%",
-                }}
-                inputContainerStyle={{
-                  backgroundColor: "whitesmoke",
-                  borderRadius: 23,
-                }}
-                leftIconContainerStyle={{ marginLeft: 16 }}
-                inputStyle={{}}
-                placeholder="Tìm kiếm nhóm.."
-                onChangeText={(Text) => {
-                  this.setState({ toSearchText: Text });
-                  this.onChangeSearchText(Text);
-                }}
-                value={this.state.toSearchText}
-              />
-            </ScrollView>
-          </View>
+              <ScrollView style={{ maxHeight: "94%" }}>
+                <SearchBar
+                  platform={Platform.OS}
+                  placeholder="Tìm bạn bè..."
+                  lightTheme="true"
+                  containerStyle={{
+                    marginHorizontal: 8,
+                    backgroundColor: "transparent",
+                    width: "95%",
+                  }}
+                  inputContainerStyle={{
+                    backgroundColor: "whitesmoke",
+                    borderRadius: 23,
+                  }}
+                  leftIconContainerStyle={{ marginLeft: 16 }}
+                  inputStyle={{}}
+                  placeholder="Tìm kiếm nhóm.."
+                  onChangeText={(Text) => {
+                    this.setState({ toSearchText: Text });
+                    this.onChangeSearchText(Text);
+                  }}
+                  value={this.state.toSearchText}
+                />
+              </ScrollView>
+            </View>
+          ) : (
+            <View
+              style={{ flexDirection: "column",height:100 }}
+              alignItems="center"
+              justifyContent="space-around"
+            >
+              <Text>Tạo nhóm mới</Text>
+              <Text>Để có thêm nhiều điều thú vị</Text>
+              <ButtonMod Text="Tạo nhóm mới" onPress={()=>this.setState({onCreate:true})}></ButtonMod>
+            </View>
+          )}
+          <View></View>
         </KeyboardAvoidingView>
       </SafeAreaView>
     );
